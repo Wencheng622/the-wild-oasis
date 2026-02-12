d717b9715724c14a759c7ea1a196ad88196e9137
关文程 <guanwencheng@guanwenchengs-MacBook-Pro.local>
2026-02-12 22:01:24 +1100

    Put an default account to let people use

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin.js";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("wencheng@test.com");
+  const [password, setPassword] = useState("123456");
   const { isLogingIn, loginAccount } = useLogin();
 
   function handleSubmit(e) {
